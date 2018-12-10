2a6647efde4b3771d1830f75258d4221337f2cc8
eV <ev@7pr.xyz>
2018-12-10 22:44:08 +0000

    fix migration camelCase and add dockerignore

--- migrations/0002_rust_crypto.down.sql
+++ migrations/0002_rust_crypto.down.sql
@@ -1,9 +1,9 @@
 drop table issuers;
 
 create table issuers (
-  issuerType text not null primary key,
+  "issuerType" text not null primary key,
   G text not null,
   H text not null,
-  privateKey text not null,
-  maxTokens integer not null
+  "privateKey" text not null,
+  "maxTokens" integer not null
 );
