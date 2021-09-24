db4b4a148b3f49efc1ac546b3751d421324fdd30
Jackson Egan <jackson@sneagan.com>
2021-09-24 16:14:13 -0400

    Upgrade go version and improve cert hack

--- kafka/main.go
+++ kafka/main.go
@@ -2,9 +2,8 @@ package kafka
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-        "encoding/json"
-        "os"
 	batgo_kafka "github.com/brave-intl/bat-go/utils/kafka"
 	"github.com/brave-intl/challenge-bypass-server/server"
 	"github.com/segmentio/kafka-go"
@@ -78,36 +77,7 @@ func StartConsumers(server *server.Server, logger *logrus.Logger) error {
 func newConsumer(topic string, groupId string, logger *logrus.Logger) *kafka.Reader {
 	var dialer *kafka.Dialer
 	brokers = strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
-        // This is a short-lived hack to allow kafka connections to work in ECS
-        // The ECS task definition sets Kafka cert information via a single JSON
-        // variable. We parse that and persist it to the file and environment
-        // variables expected by bat-go.
-	compositeCert := os.Getenv("KAFKA_SSL_CERTIFICATE")
-	if compositeCert != "" {
-		marshaledCert, err := json.Marshal(compositeCert)
-		kafkaKey, ok := marshaledCert["key"]
-		kafkaCert, ok := marshaledCert["certificate"]
-		if ok != true {
-			logger.Errorf("Failed to get certificate data from ECS environment variable.")
-		} else {
-			if err := os.WriteFile("/etc/kafkaKey", []byte(kafkaKey), 0666); err != nil {
-				logger.Error(err)
-			} else {
-				err = os.SetEnv("KAFKA_SSL_KEY_LOCATION", "/etc/kafkaKey")
-				if err != nil {
-					logger.Errorf("Failed to set key location environment variable: %e", err)
-				}
-			}
-			if err := os.WriteFile("/etc/kafkaCert", []byte(kafkaCert), 0666); err != nil {
-				logger.Error(err)
-			} else {
-				err = os.SetEnv("KAFKA_SSL_CERTIFICATE_LOCATION", "/etc/kafkaCert")
-				if err != nil {
-					logger.Errorf("Failed to set certificate location environment variable: %e", err)
-				}
-			}
-		}
-	}
+	kafkaCertHack(logger)
 	if os.Getenv("ENV") != "local" {
 		tlsDialer, _, err := batgo_kafka.TLSDialer()
 		dialer = tlsDialer
@@ -161,4 +131,41 @@ func Emit(topic string, message []byte, logger *logrus.Logger) error {
 	}
 	logger.Info("Data emitted")
 	return nil
+}
+
+/*
+ kafkaCertHack a short-lived hack to allow kafka connections to work in ECS. The ECS task
+ definition sets Kafka cert information via a single JSON variable. We parse that and
+ persist it to the file and environment variables expected by bat-go.
+*/
+func kafkaCertHack(logger *logrus.Logger) {
+	type CompositeCert struct {
+		key         string
+		certificate string
+	}
+	var compositeCert CompositeCert
+	compositeCertString := os.Getenv("KAFKA_SSL_CERTIFICATE")
+	if compositeCertString != "" {
+		err := json.Unmarshal([]byte(compositeCertString), &compositeCert)
+		if err != nil {
+			logger.Errorf("Failed to unmarshal KAFKA_SSL_CERTIFICATE. %e", err)
+		} else {
+			if err := os.WriteFile("/etc/kafka.key", []byte(compositeCert.key), 0666); err != nil {
+				logger.Error(err)
+			} else {
+				err = os.Setenv("KAFKA_SSL_KEY_LOCATION", "/etc/kafka.key")
+				if err != nil {
+					logger.Errorf("Failed to set key location environment variable: %e", err)
+				}
+			}
+			if err := os.WriteFile("/etc/kafka.cert", []byte(compositeCert.certificate), 0666); err != nil {
+				logger.Error(err)
+			} else {
+				err = os.Setenv("KAFKA_SSL_CERTIFICATE_LOCATION", "/etc/kafka.cert")
+				if err != nil {
+					logger.Errorf("Failed to set certificate location environment variable: %e", err)
+				}
+			}
+		}
+	}
 }
