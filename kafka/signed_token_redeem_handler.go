e406890e78ea7882f77737afac2326c1dd1e14b0
Jackson <jackson@sneagan.com>
2022-08-18 18:37:40 -0400

    Use logger with request id

--- kafka/signed_token_redeem_handler.go
+++ kafka/signed_token_redeem_handler.go
@@ -51,12 +51,12 @@ func SignedTokenRedeemHandler(
 		// NOTE: When we start supporting multiple requests we will need to review
 		// errors and return values as well.
 		message := fmt.Sprintf("request %s: data array unexpectedly contained more than a single message. This array is intended to make future extension easier, but no more than a single value is currently expected", tokenRedeemRequestSet.Request_id)
-		return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, log)
+		return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, &logger)
 	}
 	issuers, err := server.FetchAllIssuers()
 	if err != nil {
 		message := fmt.Sprintf("request %s: failed to fetch all issuers", tokenRedeemRequestSet.Request_id)
-		return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, log)
+		return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, &logger)
 	}
 
 	// Iterate over requests (only one at this point but the schema can support more
@@ -99,14 +99,14 @@ func SignedTokenRedeemHandler(
 		// Unmarshaling failure is a data issue and is probably permanent.
 		if err != nil {
 			message := fmt.Sprintf("request %s: could not unmarshal text into preimage", tokenRedeemRequestSet.Request_id)
-			return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, log)
+			return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, &logger)
 		}
 		verificationSignature := crypto.VerificationSignature{}
 		err = verificationSignature.UnmarshalText([]byte(request.Signature))
 		// Unmarshaling failure is a data issue and is probably permanent.
 		if err != nil {
 			message := fmt.Sprintf("request %s: could not unmarshal text into verification signature", tokenRedeemRequestSet.Request_id)
-			return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, log)
+			return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, &logger)
 		}
 		for _, issuer := range *issuers {
 			if !issuer.ExpiresAt.IsZero() && issuer.ExpiresAt.Before(time.Now()) {
@@ -135,7 +135,7 @@ func SignedTokenRedeemHandler(
 			// Unmarshaling failure is a data issue and is probably permanent.
 			if err != nil {
 				message := fmt.Sprintf("request %s: could not unmarshal issuer public key into text", tokenRedeemRequestSet.Request_id)
-				return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, log)
+				return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, &logger)
 			}
 
 			logger.Trace().
@@ -177,7 +177,7 @@ func SignedTokenRedeemHandler(
 		redemption, equivalence, err := server.CheckRedeemedTokenEquivalence(verifiedIssuer, &tokenPreimage, string(request.Binding), msg.Offset)
 		if err != nil {
 			message := fmt.Sprintf("request %s: failed to check redemption equivalence", tokenRedeemRequestSet.Request_id)
-			return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, log)
+			return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, &logger)
 		}
 
 		// Continue if there is a duplicate
@@ -241,7 +241,7 @@ func SignedTokenRedeemHandler(
 	err = resultSet.Serialize(&resultSetBuffer)
 	if err != nil {
 		message := fmt.Sprintf("request %s: failed to serialize result set", tokenRedeemRequestSet.Request_id)
-		return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, log)
+		return ResultAndErrorFromError(err, msg, message, producer, tokenRedeemRequestSet.Request_id, &logger)
 	}
 
 	return &ProcessingResult{
