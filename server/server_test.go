beb1e26c366632db24182edc05516b82fb4d547e
Ian Krieger <iandkrieger@gmail.com>
2023-08-10 18:30:57 -0400

    fix: use sprintf

--- server/server_test.go
+++ server/server_test.go
@@ -63,7 +63,7 @@ func (suite *ServerTestSuite) SetupTest() {
 	tables := []string{"v3_issuer_keys", "v3_issuers", "redemptions"}
 
 	for _, table := range tables {
-		_, err := suite.srv.db.Exec("delete from ?", table)
+		_, err := suite.srv.db.Exec(fmt.Sprintf("delete from %s", table))
 		suite.Require().NoError(err, "Failed to get clean table")
 	}
 }
